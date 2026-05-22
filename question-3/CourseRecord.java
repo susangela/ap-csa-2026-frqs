87cf9bf3d69320ca06920b1e3d02d9682ed9ad68
susangela <liwususan@gmail.com>
2026-05-22 11:42:56 -0400

    Update and rename Solution.java to CourseRecord.java

---
+++ question-3/CourseRecord.java
@@ -0,0 +1,10 @@
+public int moreHistoryThanMathAbsence(){
+  int absences = 0;
+  for (int h = 0; h < historyList.size(); h++){
+    for (int m = 0; m < mathList.size(); m++){
+      if (){
+        
+      }
+    }
+  }
+}
