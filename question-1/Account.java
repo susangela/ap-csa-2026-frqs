5ceff4b5cd13e61899176dd6462b8b568ba44434
susangela <liwususan@gmail.com>
2026-06-01 11:52:48 -0400

    Update Account.java

--- question-1/Account.java
+++ question-1/Account.java
@@ -8,5 +8,14 @@ public Account(String requestedName){
 }
 //part B
 public String getShortenedName(){
-  
+  int index = name.indexOf("-");
+  while(index! = -1){
+    if(index > 0){
+      name = name.substring(0, index - 1) + name.substring(index + 1);
+    } else {
+      name = name.substring(1);
+    }
+    index = name.indexOf("-");
+  }
+  return name;
 }
