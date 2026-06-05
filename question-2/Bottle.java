48e1b2cf8d01b8acae6382da841c3a8183a05900
susangela <liwususan@gmail.com>
2026-06-04 23:29:31 -0400

    Update Bottle.java

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,4 +1,16 @@
-public class Bottle{
-  private ;
-  private ;
+public class Bottle {
+  private double capacity;
+    private double amount;
+  
+    public Bottle(double capacity) {
+        this.capacity = capacity;
+        this.amount = capacity;
+    }
+    public double updateAmount(double removedAmount) {
+        amount -= removedAmount;
+        if (amount < capacity * 0.25) {
+            amount = capacity;
+        } 
+        return amount;
+    }
 }
